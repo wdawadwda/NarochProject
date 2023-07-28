94f8b03310b60883d98229315140c8c0409398ee
wdawadwda <wdawadwdawork@gmail.com>
2023-07-28 15:15:22 +0300

    fix: private route

--- Naroch/admin_for_staff/urls.py
+++ Naroch/admin_for_staff/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import user_passes_test
+
+def is_admin(user):
+    return user.is_authenticated and user.is_staff
 
 urlpatterns = [
-    path('', views.admin_for_staff, name="admin_for_staff"),
-    path('comment_moderation/', views.comment_moderation, name="comment_moderation"),
-    path('create_comment/', views.create_comment, name="create_comment"),
-    path('comment_moderation/<int:pk>/', views.comment_detail, name='comment_detail'),
-    path('comment_moderation/<int:pk>/update', views.CommentUpdateView.as_view(), name='comment_update'),
-    path('comment_moderation/<int:pk>/delite', views.CommentDeliteView.as_view(), name='comment_delite'),
+    path('', user_passes_test(is_admin)(views.admin_for_staff), name="admin_for_staff"),
+    path('comment_moderation/', user_passes_test(is_admin)(views.comment_moderation), name="comment_moderation"),
+    path('create_comment/', user_passes_test(is_admin)(views.create_comment), name="create_comment"),
+    path('comment_moderation/<int:pk>/', user_passes_test(is_admin)(views.comment_detail), name='comment_detail'),
+    path('comment_moderation/<int:pk>/update', user_passes_test(is_admin)(views.CommentUpdateView.as_view()), name='comment_update'),
+    path('comment_moderation/<int:pk>/delite', user_passes_test(is_admin)(views.CommentDeliteView.as_view()), name='comment_delite'),
 
-    path('gallery_moderation/', views.gallery_moderation, name="gallery_moderation"),
-    path('create_photo/', views.create_photo, name="create_photo"),
-    path('gallery_moderation/<int:pk>/', views.gallery_detail, name='gallery_detail'),
-    path('gallery_moderation/<int:pk>/update', views.GalleryUpdateView.as_view(), name='photo_update'),
-    path('gallery_moderation/<int:pk>/delite', views.GalleryDeliteView.as_view(), name='photo_delite'),
+    path('gallery_moderation/', user_passes_test(is_admin)(views.gallery_moderation), name="gallery_moderation"),
+    path('create_photo/', user_passes_test(is_admin)(views.create_photo), name="create_photo"),
+    path('gallery_moderation/<int:pk>/', user_passes_test(is_admin)(views.gallery_detail), name='gallery_detail'),
+    path('gallery_moderation/<int:pk>/update', user_passes_test(is_admin)(views.GalleryUpdateView.as_view()), name='photo_update'),
+    path('gallery_moderation/<int:pk>/delite', user_passes_test(is_admin)(views.GalleryDeliteView.as_view()), name='photo_delite'),
 ]
